fdf19b3e9468e1dcb2504fca6fb2931b4445affd
Jeremy Schwartz <j.schwartz564@icloud.com>
2020-03-03 14:38:41 -0800

    Update Carry Functions To Support N-Bit Integers

--- lib/bix/carry.ex
+++ lib/bix/carry.ex
@@ -10,20 +10,7 @@ defmodule Bix.Carry do
   use Bitwise, only_operators: true
 
   @doc """
-  Guard for integers which can be stored in a single byte.
-  """
-  defguard is_byte(x) when is_integer(x) and x >= 0 and x < 256
-
-  @doc """
-  An integer which is either 0 or 1.
-  """
-  defguard is_bit(x) when is_integer(x) and (x == 0 or x == 1)
-
-  defguardp are_bytes(x, y) when is_byte(x) and is_byte(y)
-  defguardp are_bytes(x, y, z) when is_byte(x) and is_byte(y) and is_byte(z)
-
-  @doc """
-  Byte addition with carry-in and carry-out.
+  N-bit addition with carry-in and carry-out. Assuming N <= 8.
 
   ## Examples
 
@@ -36,19 +23,24 @@ defmodule Bix.Carry do
       iex> Bix.Carry.add(255, 3, 1)
       {3, 1}
 
+  The `bit_width` parameter determines the width of the add. For example a 3-bit
+  add can be performed like so:
+
+      iex> Bix.Carry.add(0x07, 0x01, 0, 3)
+      {0, 1}
+
   """
-  @spec add(byte, byte, integer) :: {byte, integer}
-  def add(a, b, carry_in) when are_bytes(a, b) and is_bit(carry_in) do
-    carry_gen = a &&& b
-    carry_prop = a ||| b
-    sum = :binary.first <<a + b + carry_in :: 8>>
-    carry_out = carry_gen ||| (carry_prop &&& ~~~sum)
-    carry_out = carry_out >>> 7
-    {sum, carry_out}
+  @spec add(byte, byte, byte, 1..8) :: {byte, byte}
+  def add(a, b, carry_in, bit_width \\ 8) do
+    sum = a + b + carry_in
+    carry_out = sum >>> bit_width
+    sum_trunc = Bix.Binary.first(<<sum::size(bit_width)>>)
+    {sum_trunc, carry_out}
   end
 
   @doc """
-  Shifts the bits of `x` to the left `s` number of positions.
+  Shifts the bits of `x` to the left `s` number of positions truncating the 
+  result to a specific `bit_width`.
 
   Only shifts of less than 8 bits at a time are supported at the moment.
 
@@ -57,14 +49,15 @@ defmodule Bix.Carry do
       iex> Bix.Carry.bsl(0xaf, 3, 0x02)
       {0x7a, 0x05}
 
-  """
-  @spec bsl(byte, 0..7, byte) :: {byte, byte}
-  def bsl(x, s, carry_in) when are_bytes(x, s, carry_in) and s < 8 do
-    carry_out = x >>> (8 - s)
-    result = truncate_byte(x <<< s) ||| carry_in
-    {result, carry_out}
-  end
+      iex> Bix.Carry.bsl(0x0f, 3, 0x02, 4)
+      {0x0a, 0x07}
 
-  @spec truncate_byte(integer) :: byte
-  defp truncate_byte(x), do: :binary.first <<x :: 8>>
+  """
+  @spec bsl(byte, 0..7, byte, 1..8) :: {byte, byte}
+  def bsl(x, s, carry_in, bit_width \\ 8) when s < 8 do
+    carry_out = x >>> (bit_width - s)
+    result = x <<< s ||| carry_in
+    result_trunc = Bix.Binary.first(<<result::size(bit_width)>>)
+    {result_trunc, carry_out}
+  end
 end
